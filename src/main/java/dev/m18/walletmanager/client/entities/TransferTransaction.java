ca5e4ac9994c3a37a77b36f87628714eef24a7fb
EU99 <eu99@18m.dev>
2022-10-11 09:56:53 +0800

    feat: add fields walletName, walletTag, clientTag and refNo in TransferTransaction.

--- src/main/java/dev/m18/walletmanager/client/entities/TransferTransaction.java
+++ src/main/java/dev/m18/walletmanager/client/entities/TransferTransaction.java
@@ -5,7 +5,6 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import dev.m18.walletmanager.client.enums.ChainType;
 import dev.m18.walletmanager.client.enums.Direction;
 import dev.m18.walletmanager.client.enums.TransactionStatus;
 import dev.m18.walletmanager.client.enums.TransactionType;
@@ -34,6 +33,18 @@ public class TransferTransaction {
 	
 	@JsonProperty("block_number")
 	Long blockNumber;
+	
+	@JsonProperty("wallet_name")
+	String walletName;
+	
+	@JsonProperty("wallet_tag")
+	String walletTag;
+	
+	@JsonProperty("client_tag")
+	String clientTag;
+	
+	@JsonProperty("ref_no")
+	String refNo;
 	
 	@JsonProperty("asset_name")
 	String assetName;
